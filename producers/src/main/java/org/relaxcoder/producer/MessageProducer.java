532f1c7d314183e67bdb0d4fd227ee017ff228a1
Himanshu Pandey <himanshupandey2411@gmail.com>
2023-07-05 14:25:26 +0530

    added command line launcher

--- producers/src/main/java/org/relaxcoder/producer/MessageProducer.java
+++ producers/src/main/java/org/relaxcoder/producer/MessageProducer.java
@@ -20,6 +20,21 @@ public class MessageProducer {
         this.kafkaProducer = new KafkaProducer<>(propMap);
     }
 
+    Callback callback = (recordMetadata, exception) -> {
+      if(exception != null){
+          logger.error("Exception is {}", exception.getMessage());
+      }
+      else{
+          logger.info("Record Metadata Async in Callback Offset: {}  and the partition is {} ",
+                  recordMetadata.offset(),
+                  recordMetadata.partition()
+          );
+      }
+    };
+
+    public void close(){
+        kafkaProducer.close();
+    }
     public static Map<String, Object> propsMap(){
         Map<String, Object> propMap = new HashMap<>();
         propMap.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092, localhost:9093, localhost:9094");
@@ -40,15 +55,20 @@ public class MessageProducer {
         }
     }
 
+    public void publishMessageAsync(String key, String message){
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName, key, message);
+        kafkaProducer.send(producerRecord, callback);
+    }
     public void logSuccessResponse(String message, String key, RecordMetadata recordMetadata){
         logger.info("Message **{}** sent successfully with the key **{}** .", message, key);
         logger.info("Published Record Offset is {} and the partition is {}",
                 recordMetadata.offset(), recordMetadata.partition());
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Map<String, Object> producerProps = propsMap();
         MessageProducer messageProducer = new MessageProducer(producerProps);
-        messageProducer.publishMessageSync(null, "ABC");
-        messageProducer.publishMessageSync(null, "DEF");
+        messageProducer.publishMessageAsync("99", "Him");
+        messageProducer.publishMessageAsync("99", "Pan");
+        Thread.sleep(5000);
     }
 }
